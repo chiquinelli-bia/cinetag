020faaf39f9d928fdc01ead22884ff9b534869aa
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-04-02 08:13:01 -0300

    feat(cabecalho): cria componente Cabecalho e CabecalhoLink com estilos

---
+++ src/components/cabecalho/cabecalho.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import styles from "./cabecalho.module.css";
+import CabecalhoLink from "./cabecalhoLink/cabecalhoLink";
+function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <Link to="/">
+        <img src={logo} alt="logo do cinetag" />
+      </Link>
+      <nav>
+        <CabecalhoLink url="/">Home</CabecalhoLink>
+        <CabecalhoLink url="./Favoritos">Favoritos</CabecalhoLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Cabecalho;
