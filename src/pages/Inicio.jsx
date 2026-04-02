450c7d9d49435e52903b49df402e4de36f2ea237
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-04-02 08:40:06 -0300

    feat(Banner): cria componente e importa

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,10 +1,12 @@
 import Rodape from "../components/rodape/rodape.jsx";
 import Cabecalho from "../components/cabecalho/cabecalho.jsx";
+import Banner from "../components/banner/banner.jsx";
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="home" />
       <Rodape />
     </>
   );
