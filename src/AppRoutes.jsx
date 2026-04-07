b5a8c29fff120451c6758c256ef5cab0f68d2567
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-04-07 08:55:32 -0300

    refactor(pages): fixa Cabecalho e Rodape nas páginas

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,21 @@
-import Inicio from "@/pages/Inicio";
+import Inicio from "@/pages/inicio/inicio.jsx";
+import Cabecalho from "@/components/cabecalho/cabecalho.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Rodape from "@/components/rodape/rodape.jsx";
+import Container from "@/components/container/container.jsx";
+import Favoritos from "@/pages/favoritos/favoritos";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-      </Routes>
+      <Cabecalho />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/favoritos" element={<Favoritos />}></Route>
+        </Routes>
+      </Container>
+      <Rodape />
     </BrowserRouter>
   );
 }
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,5 +1,3 @@
-import Rodape from "@/components/rodape/rodape.jsx";
-import Cabecalho from "@/components/cabecalho/cabecalho.jsx";
 import Banner from "@/components/banner/banner.jsx";
 import Titulo from "@/components/titulo/titulo.jsx";
 import videos from "./db.json";
@@ -9,12 +7,10 @@ import Card from "@/components/card/card.jsx";
 function Inicio() {
   return (
     <>
-      <Cabecalho />
       <Banner imagem="home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Rodape />
       <section className={styles.container}>
         {videos.map((video) => {
           return <Card {...video} key={video.id}></Card>;
