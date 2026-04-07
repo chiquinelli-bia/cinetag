4c574300b7382b728db78ac17c25006d4a8f89e0
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-04-07 08:28:11 -0300

    feat(início): renderiza componente Card via mapeamento de array e aplica estilos CSS

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,6 +2,9 @@ import Rodape from "../components/rodape/rodape.jsx";
 import Cabecalho from "../components/cabecalho/cabecalho.jsx";
 import Banner from "../components/banner/banner.jsx";
 import Titulo from "../components/titulo/titulo.jsx";
+import videos from "./db.json";
+import styles from "./inicio.module.css";
+import Card from "../components/card/card.jsx";
 
 function Inicio() {
   return (
@@ -12,6 +15,11 @@ function Inicio() {
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
       <Rodape />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id}></Card>;
+        })}
+      </section>
     </>
   );
 }
